34a06e22ca81322754566b847a1dbc32f4335a84
gztrk-endineer <gztrk@engineer.com>
2023-10-19 12:24:22 +0400

    Return blockchain on a page

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask 
+from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
+blockchain = Blockchain()
+for i in range(3):
+    blockchain.addBlock(i+1)
 
 @app.route("/")
 def default():
     return "<h1>Welcome To Blockchain</h1>"
+
+@app.route("/blockchain")
+def routeBlockchain():
+    # Need to return string, dict, tuple, Response instance or WSGI callable
+    return blockchain.__repr__()
 
 app.run(port=5001)
