33bc263f8b4cb68ade17089c09421cbbd5210f4f
gztrk-endineer <gztrk@engineer.com>
2023-10-19 14:44:24 +0400

    Add mine endpoint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,8 +3,8 @@ from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
 blockchain = Blockchain()
-for i in range(3):
-    blockchain.addBlock(i+1)
+# for i in range(3):
+#     blockchain.addBlock(i+1)
 
 @app.route("/")
 def default():
@@ -17,6 +17,12 @@ def routeBlockchain():
     # return blockchain.__repr__()
     # Or jsonify(bch) -> server error (bch not serializable)
     return jsonify(blockchain.toJson()) 
+
+@app.route("/blockchain/mine")
+def routeBlockchainMine():
+    transactionData = "stubbed_transaction_data"
+    blockchain.addBlock(transactionData)
+    return jsonify(blockchain.chain[-1].toJson())
 
 app.run()
 # app.run(port=5001)
